714db84e02277e01f752b86972347856add8e2e0
Mustapha mensouri <mestaphamansouri@gmail.com>
2023-05-31 16:55:17 +0100

    connect the Tools class with dbase

--- app/src/main/java/com/example/medailast/Tools.java
+++ app/src/main/java/com/example/medailast/Tools.java
@@ -10,15 +10,29 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.medailast.Adapters.AdapterTools;
+import com.example.medailast.Adapters.EmployeeAdapter;
+import com.example.medailast.Models.EmployeeModel;
 import com.example.medailast.Models.ToolsModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class Tools extends AppCompatActivity {
     private RecyclerView all_tools;
+    private ArrayList<ToolsModel> list_tools;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +45,51 @@ public class Tools extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //        -----------------------------------------
+        list_tools = new ArrayList<>();
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.21/rest api/api/post tools/post.php";
+//        get the data from the server
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
 
-        ArrayList<ToolsModel> list_tools = new ArrayList<>();
-        list_tools.add(new ToolsModel("hammer", "12/07/2023"));
-        list_tools.add(new ToolsModel("hammer23", "01/07/2023"));
-        list_tools.add(new ToolsModel("hammer33", "28/07/2023"));
-        list_tools.add(new ToolsModel("hammer55", "22/07/2023"));
-        list_tools.add(new ToolsModel("hammer4", "02/07/2023"));
+                for (int i=0; i<response.length(); i++){
+                    String num = "";
+                    String returnit = "";
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        num = jsonObject.getString("nameTool");
+                        returnit = jsonObject.getString("returnit");
+                        if (returnit.equals("1")){
+                            list_tools.add(new ToolsModel(num, "has been returned"));
+                        }else{
+                            list_tools.add(new ToolsModel(num, jsonObject.getString("return_date")));
+                        }
 
-        AdapterTools adapterTools = new AdapterTools(this);
-        adapterTools.setTool_list(list_tools);
 
-        all_tools.setAdapter(adapterTools);
-        all_tools.setLayoutManager(new LinearLayoutManager(this));
+
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                AdapterTools adapterTools = new AdapterTools(Tools.this);
+                adapterTools.setTool_list(list_tools);
+
+                all_tools.setAdapter(adapterTools);
+                all_tools.setLayoutManager(new LinearLayoutManager(Tools.this));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(Tools.this, "error the internet", Toast.LENGTH_SHORT).show();;
+            }
+        });
+        queue.add(request);
+
+
+
 
     }
 
